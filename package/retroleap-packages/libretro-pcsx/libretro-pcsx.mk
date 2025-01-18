bba5957d2515ec509cc9d23dd10af3b7a8be065d
DCFUKSURMOM <danielcook30a@gmail.com>
2025-01-18 01:00:10 -0600

    fixed pcsxr (ps1) core

--- package/retroleap-packages/libretro-pcsx/libretro-pcsx.mk
+++ package/retroleap-packages/libretro-pcsx/libretro-pcsx.mk
@@ -5,9 +5,9 @@
 ################################################################################
 LIBRETRO_PCSX_VERSION = 3fc26d1c9ff2b2cf236d10a20cd02b84ca858de2
 LIBRETRO_PCSX_SITE = $(call github,libretro,pcsx_rearmed,$(LIBRETRO_PCSX_VERSION))
-
+LIBRETRO_PCSX_DEPENDS = retroarch
 define LIBRETRO_PCSX_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_BOARD)"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile.libretro platform=unix
 endef
 
 define LIBRETRO_PCSX_INSTALL_TARGET_CMDS
